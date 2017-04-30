824be59bfa84acb7f3cb6dc69a0a70a165fc4dd8
Jack Zuban <jack.zuban@gmail.com>
2017-04-30 03:04:36 -0500

    add parameters checking for routes

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,14 @@
 
 Route::group(['namespace' => 'Api', 'middleware' => ['api']], function () {
     Route::get('/events', ['uses' => 'EventsController@index']);
-    Route::get('/events/{id}', ['uses' => 'EventsController@show']);
+
+    Route::get('/events/{id}', ['uses' => 'EventsController@show'])
+        ->where('id', '[0-9]+');
+
     Route::post('/events', ['uses' => 'EventsController@store']);
+
     Route::put('/events/{id}', ['uses' => 'EventsController@update']);
-    Route::delete('/events/{id}', ['uses' => 'EventsController@delete']);
+
+    Route::delete('/events/{id}', ['uses' => 'EventsController@delete'])
+        ->where('id', '[0-9]+');
 });
